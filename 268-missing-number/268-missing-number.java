11d981e42203fb199dd619b6141eb3b3f8c85fb2
Ravi Prakash <84272788+i-am-ravi-prakash@users.noreply.github.com>
2022-07-09 08:27:13 +0530

    Time: 1 ms (72.69%), Space: 51 MB (51.93%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int idealSum = len*(len + 1)/2;
+        int currSum = 0;
+        
+        for(int i = 0; i < len; i++){
+            currSum += nums[i];
+        }
+        
+        return idealSum - currSum;
+    }
+}
