af8d83820a87cc38981d9b1453c43dd5caeb3309
Ravi Prakash <84272788+i-am-ravi-prakash@users.noreply.github.com>
2022-07-17 15:58:18 +0530

    Time: 18 ms (13.54%), Space: 51.5 MB (8.12%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public List<Integer> majorityElement(int[] nums) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < len; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], 1);
+            } else {
+                map.put(nums[i], (map.get(nums[i]) + 1));
+            }
+        }
+        
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() > (len/3)){
+                result.add(entry.getKey());
+            }
+        }
+        
+        return result;
+    }
+}
