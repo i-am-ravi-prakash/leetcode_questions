429568f4c31b64fd36ffd61a9de195e3be535c89
Ravi Prakash <84272788+i-am-ravi-prakash@users.noreply.github.com>
2022-07-18 08:52:58 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (55.22%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,5 +1,27 @@
 class Solution {
+
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int low = 0;
+        int high = (nums.length) - 1;
+        int i = 0;
+
+        while (i <= high) {
+            if (nums[i] == 0) {
+                swap(nums, i, low);
+                i++;
+                low++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, high);
+                high--;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public void swap(int[] arr, int low, int high) {
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
     }
 }
