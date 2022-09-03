fc9df0c5e20acc1103b6a028d3da07236bbf2569
Ravi Prakash <84272788+i-am-ravi-prakash@users.noreply.github.com>
2022-09-03 13:54:20 +0530

    Time: 1 ms (97.66%), Space: 43.8 MB (93.21%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,5 +1,6 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        /**
         int row = matrix.length;
         int col = matrix[0].length;
         
@@ -22,5 +23,23 @@ class Solution {
                 }
             }
         }
+    }
+    */
+        
+        int m = matrix.length, n = matrix[0].length, k = 0;
+    // First row has zero?
+    while (k < n && matrix[0][k] != 0) ++k;
+    // Use first row/column as marker, scan the matrix
+    for (int i = 1; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            if (matrix[i][j] == 0)
+                matrix[0][j] = matrix[i][0] = 0;
+    // Set the zeros
+    for (int i = 1; i < m; ++i)
+        for (int j = n - 1; j >= 0; --j)
+            if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                matrix[i][j] = 0;
+    // Set the zeros for the first row
+    if (k < n) Arrays.fill(matrix[0], 0);
     }
 }
