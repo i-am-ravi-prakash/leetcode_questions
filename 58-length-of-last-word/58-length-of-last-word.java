5d6ff91c5ae59b26a818220c64715f72b363b04f
Ravi Prakash <84272788+i-am-ravi-prakash@users.noreply.github.com>
2022-09-16 21:48:07 +0530

    Time: 1 ms (51.84%), Space: 42.2 MB (50.17%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,23 +1,28 @@
 class Solution {
 
-    public int lengthOfLastWord(String s) {
-        int len = s.length();
-        int lastWordLength = 0;
-        int i = len - 1;
-        char curr = ' ';
-        char prev = ' ';
-
-        while (i >= 0) {
-            curr = s.charAt(i);
-            if (curr == ' ') {
-                if (curr == prev) i--; else break;
-            } else {
-                lastWordLength++;
-                i--;
+    public boolean validtaeString(String str) {
+        str = str.toLowerCase();
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char ch = charArray[i];
+            if (!(ch >= 'a' && ch <= 'z')) {
+                return false;
             }
-            prev = curr;
         }
+        return true;
+    }
 
-        return lastWordLength;
+    public int lengthOfLastWord(String s) {
+        int counter = 0;
+        for (int i = s.length() - 1; i > 0; i--) {
+            if (s.charAt(i) == ' ') {
+                continue;
+            } else if (validtaeString(String.valueOf(s.charAt(i))) && s.charAt(i - 1) != ' ') {
+                counter++;
+            } else {
+                break;
+            }
+        }
+        return counter + 1;
     }
 }
