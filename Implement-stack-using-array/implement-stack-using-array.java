ce611e41269b50186e66c2deaa764aa7d4b3e19d
Ravi Prakash <84272788+i-am-ravi-prakash@users.noreply.github.com>
2022-11-11 07:43:42 +0530

    formatted code

--- Implement-stack-using-array/implement-stack-using-array.java
+++ Implement-stack-using-array/implement-stack-using-array.java
@@ -1,39 +1,38 @@
-class MyStack{
-  int top;
-	int arr[] = new int[1000];
-
-  MyStack(){
-		top = -1;
-	}
-	
-	// push an integer into the stack.
-  void push(int a){
-	    arr[++top] = a;
-	} 
-	
-  // removes an item from top of the stack.
-	int pop(){
-	    int poppedEle = -1;
-	    if(top != -1){
-	        poppedEle = arr[top];
+class MyStack {
+    int top;
+    int arr[] = new int[1000];
+    
+    MyStack() {
+        top = -1;
+    }
+    
+    // push an integer into the stack.
+    void push(int a) {
+        arr[++top] = a;
+    }
+    
+    // removes an item from top of the stack.
+    int pop() {
+        int poppedEle = -1;
+        if (top != -1) {
+            poppedEle = arr[top];
             top--;
-	    }
-        
+        }
         return poppedEle;
-	}
-  
-  // returns size of the stack
-  int size(){
+    }
+    
+    // returns size of the stack
+    int size() {
         return (top + 1);
     }
     
-  // returns top element of the stack
-  int top(){
-      return arr[top];
-  }
-
-  // return true if stack is empty, false otherwise
-  boolean isEmpty(){
-      return (top == -1);
-  }
+    // returns top element of the stack
+    int top() {
+        return arr[top];
+    }
+    
+    // return true if stack is empty, false otherwise
+    boolean isEmpty() {
+        return (top == -1);
+    }
 }
